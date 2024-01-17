1d437c3aee9a48c22935704d80efc9a45d1f0b0d
Nameson Gaudel <namesongaudel.ng@gmail.com>
2024-01-17 14:15:08 -0500

    add: timeline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { Hero } from "~/components/Hero/Hero";
 import { FeaturesSection } from "~/components/FetureSection/FetureSectin";
 import CheckListSection from "~/components/CheckListSection/CheckListSection";
 import { ReactElement } from "react";
+import { HowItWorksTimeline } from "~/components/TimeLine/HowItWorksTimeline";
 export default function Home() {
   return (
     <div>
       <Hero />
       <FeaturesSection />
+      <HowItWorksTimeline />
       <CheckListSection />
     </div>
   );
